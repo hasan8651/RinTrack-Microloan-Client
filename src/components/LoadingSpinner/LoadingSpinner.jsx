f29c120decd793ed80a72c81947e330ff808121e
hasan8651 <hasan865@gmail.com>
2025-12-17 22:42:45 +0600

    fixing loading spinner

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
   return (
-    <div className="h-80 flex items-center justify-center bg-orange-50 dark:bg-transparent">
+    <div className="h-80 flex items-center justify-center">
       <div className="relative flex items-center justify-center">
         <div className="w-40 h-40 rounded-full border-4 border-blue-500/30 border-t-blue-500 animate-spin"></div>
         <div className="absolute inset-0 flex items-center justify-center">
